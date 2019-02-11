5a425eee75ef50a178deeb600c046cd02afdf653
Quinten Steenhuis <qsteenhuis@gmail.com>
2019-02-11 14:17:28 +0000

    Improvements

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import os
 import sys
 from setuptools import setup, find_packages
@@ -45,9 +43,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MACourts',
-      version='0.0.2',
+      version='0.0.6',
       description=('List Massachusetts Courts in Docassemble'),
-      long_description='A utility package that includes JSON files representing all of the courts in Massachusetts.\r\n\r\nCourts automatically scraped from Mass.gov and geocoded with Google Maps, so addresses\r\nmay not always be properly entered (esp. for PO Boxes)',
+      long_description=u'A utility package that includes JSON files representing all of the courts in Massachusetts.\r\n\r\nCourts automatically scraped from Mass.gov and geocoded with Google Maps, so addresses\r\nmay not always be properly entered (esp. for PO Boxes)',
       long_description_content_type='text/markdown',
       author='Quinten Steenhuis',
       author_email='qsteenhuis@gbls.org',
